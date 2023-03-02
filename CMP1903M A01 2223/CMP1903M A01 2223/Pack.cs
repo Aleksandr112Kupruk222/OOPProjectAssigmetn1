7f15e77d42a92a449407c0f08da10830a877ddca
Aleksandr112Kupruk222 <33401011+Aleksandr112Kupruk222@users.noreply.github.com>
2023-03-02 22:41:53 +0000

    Add files via upload
    Base code for the assigment

---
+++ CMP1903M A01 2223/CMP1903M A01 2223/Pack.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903M_A01_2223
+{
+    class Pack
+    {
+        List<Card> pack = new List<Card>();
+
+        public Pack()
+        {
+            //Initialise the card pack here
+        }
+
+        public static bool ShuffleCardPack(int typeOfShuffle)
+        {
+            //Shuffles the pack based on the type of shuffle
+
+        }
+        public static Card Deal()
+        {
+            //Deals one card
+
+        }
+        public static List<Card> DealCard(int amount)
+        {
+            //Deals the number of cards specified by 'amount'
+        }
+
+
+
+
+
+
+
+    }
+}
